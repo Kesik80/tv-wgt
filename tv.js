e5c5570092e7780048894680dbde1e7f1c1301d4
Kesik80 <55840881+Kesik80@users.noreply.github.com>
2026-08-04 20:09:27 +0200

    Upload 1 file(s)

--- tv.js
+++ tv.js
@@ -12,7 +12,7 @@ function tvFetchRuntime(){
       var origin=new URL(url).origin;
       var head='<base href="'+origin+'/"><style>html{background:#000}</style>';
       var tail='<scr'+'ipt>addEventListener("load",function(){try{var w=document.body.scrollWidth||1280;var k=window.innerWidth/w;if(k>1.05)document.documentElement.style.zoom=k.toFixed(2);}catch(e){}});</scr'+'ipt>';
-      tail+='<scr'+'ipt>@@NAV@@</scr'+'ipt>';
+      tail+='<scr'+'ipt>'+NAVSRC+'</scr'+'ipt>';
       var hm=html.match(/<head[^>]*>/i);
       if(hm){var i1=hm.index+hm[0].length;html=html.slice(0,i1)+head+html.slice(i1);}else{html=head+html;}
       var be=html.match(/<\/body>/i);
@@ -41,6 +41,7 @@ function tvFetchRuntime(){
   }catch(e){direct();}
 }
 indexDirect=function(name,url){
-  var src=tvFetchRuntime.toString().replace('@@NAV@@',spatialNavRuntime.toString());
-  return '<!DOCTYPE html><html><head><meta charset="utf-8"><title>'+name+'</title></head><body data-url="'+url+'" style="margin:0;background:#000;color:#fff;font-family:sans-serif;display:flex;align-items:center;justify-content:center;height:100vh"><div style="text-align:center"><div style="font-size:32px;margin-bottom:14px">Загрузка…</div><div id="tvMsg" style="color:#888;font-size:16px">'+name+'</div></div><scr'+'ipt>('+src+')();</scr'+'ipt></body></html>';
+  var navLit=JSON.stringify(spatialNavRuntime.toString());
+  var src=tvFetchRuntime.toString();
+  return '<!DOCTYPE html><html><head><meta charset="utf-8"><title>'+name+'</title></head><body data-url="'+url+'" style="margin:0;background:#000;color:#fff;font-family:sans-serif;display:flex;align-items:center;justify-content:center;height:100vh"><div style="text-align:center"><div style="font-size:32px;margin-bottom:14px">Загрузка…</div><div id="tvMsg" style="color:#888;font-size:16px">'+name+'</div></div><scr'+'ipt>var NAVSRC='+navLit+';('+src+')();</scr'+'ipt></body></html>';
 };
